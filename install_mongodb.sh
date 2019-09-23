5fe6bdb599533909ff0c9eaa75c70e62aaf774d8
Sergey Karakulanov <sserdj7@gmail.com>
2019-09-23 23:18:05 +0300

    Edited install_mongodb.sh file

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -4,7 +4,7 @@
 # Written by sserdj&@gmail.com for OTUS
 ###########
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt-get update
 sudo apt-get install -y mongodb-org
